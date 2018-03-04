09958bcd5b889d8069517802374144256e0674b8
Oleksandr Chzhen <oleksandr.chzhen@gmail.com>
2018-03-04 10:22:27 +0200

    Slightly change project structure, add simple models

--- config.py
+++ config.py
@@ -2,3 +2,10 @@ import os
 
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+SECRET_KEY = os.environ.get('MOZ_SECRET_KEY', 'SUPER_SECRET_KEY')
+
+DB_NAME = 'sql11223417'
+DB_USER = 'sql11223417'
+DB_PASSWORD = os.environ.get('DB_PWD')
+DB_HOST = 'sql11.freemysqlhosting.net'
+DB_PORT = '3306'
--- run.py
+++ run.py
@@ -1,8 +1,6 @@
-from flask import Flask
-from main.views import main
+from app import app
 from config import DEBUG
 
-app = Flask(__name__)
-app.register_blueprint(main)
+
 if __name__ == '__main__':
     app.run(debug=DEBUG)
