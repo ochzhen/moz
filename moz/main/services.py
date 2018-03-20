802d928f060ae6a74de4616074a6b5d97703ca7e
Arsenii Zhdanov <arsen2@ukr.net>
2018-03-20 23:05:34 +0200

    Fixes (#16)
    * fix search to be case insesitive
    
    * Fix search(not works in sqltie)
    
    * fix media directory not exist problem

--- moz/main/services.py
+++ moz/main/services.py
@@ -15,8 +15,8 @@ def get_categories_with_documents():
 
 def get_documents_for_query(query):
     if type(query) == str or type(query) == unicode:
-        query = u'%%%s%%' % query  # will build string %query%
-        documents = MOZDocument.select().where(MOZDocument.title ** query).order_by(MOZDocument.title.asc())
+        query = u'%%%s%%' % query
+        documents = MOZDocument.select().where(MOZDocument.title.contains(query)).order_by(MOZDocument.title.asc())
         if not documents or len(documents) == 0:
             return []
         return documents
