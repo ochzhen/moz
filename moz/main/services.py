1a5b0d3c400e3bcf79914b2b5d66e95460e3e25c
Arsenii Zhdanov <arsenzhd@gmail.com>
2018-03-20 21:07:45 +0200

    fix search to be case insesitive

--- moz/main/services.py
+++ moz/main/services.py
@@ -17,8 +17,7 @@ def get_categories_with_documents():
 
 def get_documents_for_query(query):
     if type(query) == str or type(query) == unicode:
-        query = u'%%%s%%' % query  # will build string %query%
-        documents = MOZDocument.select().where(MOZDocument.title ** query).order_by(MOZDocument.title.asc())
+        documents = MOZDocument.select().where(MOZDocument.title.contains(query)).order_by(MOZDocument.title.asc())
         if not documents or len(documents) == 0:
             return []
         return documents
